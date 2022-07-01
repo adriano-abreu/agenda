1696edceb9422d36751d590895f2e4ad54f50223
adriano-abreu <adrianodabreu@gmail.com>
2022-07-01 18:57:03 -0300

    main

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const { Module } = require("module");
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./frontend/index.js",
+  output: {
+    path: path.resolve(__dirname, "public", "assets", "js"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/env"],
+          },
+        },
+      },
+    ],
+  },
+  devtool: "source-map",
+};
